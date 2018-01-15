699ca70003728cf59ad01b2c9f76b031e4e528a6
Asif Malik <asif.malik@gmail.com>
2018-01-15 22:46:37 +0000

    Rowling

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -7,6 +7,9 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("rowling")) {
+            return "Joanne Rowling, CH, OBE, FRSL, FRCPE, who writes under the pen names J. K. Rowling and Robert Galbraith, is a British novelist and screenwriter who is best known for writing the Harry Potter fantasy series.";
+        }
         return "";
     }
 }
